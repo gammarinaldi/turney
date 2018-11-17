d092288edd96a04fa3fddfab9a3a4284b8bb7ada
gammarinaldi <38407508+gammarinaldi@users.noreply.github.com>
2018-11-17 22:21:29 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import Header from './components/Header';
+import Search from './components/Search';
+import Info from './components/Info';
+import Carousel from './components/Carousel';
+import SideBar from './components/SideBar';
+import LatestEvents from './components/LatestEvents';
+import FeaturedEvents from './components/FeaturedEvents';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+
+        <Row>
+          {/* HEADER */}
+          <Col>
+            <Header />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          {/* INFO */}
+          <Col>
+            <Info />
+          </Col>
+        </Row>
+
+        <Row>
+          {/* CAROUSEL */}
+          <Col><center><Carousel /></center></Col>
+        </Row>
+        <br/>
+        <Row>
+          {/* SEARCH */}
+          <Col><center><Search /></center></Col>
+        </Row>
+        <br/>
+        <Row>
+          {/* SIDEBAR */}
+          <Col xs="3">
+            <SideBar />
+          </Col>
+
+          <Col xs="1">
+            &nbsp;&nbsp;&nbsp;&nbsp;
+          </Col>
+
+          {/* CONTENT */}
+          <Col xs="8">
+            <h3>Latest Events</h3>
+            <LatestEvents />
+            <br/>
+            <h3>Featured Events</h3>
+            <FeaturedEvents />
+          </Col>
+        </Row>
+
+      </Container>
     );
   }
 }
